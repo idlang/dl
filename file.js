1094255637645291a71f4ddc860ceedb2fcd25c9
Gilang ID <30952541+idlang@users.noreply.github.com>
2017-09-23 23:19:13 +0700

    Create file.js

---
+++ file.js
@@ -0,0 +1,14 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function()
+{
+var aa=$("#jangan").val();
+if (aa == null) {
+window.location.href = "http://langdl.blogspot.com/";
+};
+$("#jangan").attr("href","
+http://langdl.blogspot.com/");
+
+});
+//]]>
+</script>
